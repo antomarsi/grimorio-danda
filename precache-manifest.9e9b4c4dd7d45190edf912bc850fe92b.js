12f878bf9d57d73a654a0b655b51427d799abadb
Antonio Marco da Silva <antomarsi@hotmail.com>
2019-01-30 22:18:11 -0200

    publicação do ghpages

---
+++ precache-manifest.9e9b4c4dd7d45190edf912bc850fe92b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/grimorio-danda/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "739cf522e450259cd381",
+    "url": "/grimorio-danda/static/css/main.7eba449f.chunk.css"
+  },
+  {
+    "revision": "c243adb601cb0e965344851b1c46194f",
+    "url": "/grimorio-danda/static/media/rpgawesome-webfont.c243adb6.svg"
+  },
+  {
+    "revision": "39dc38292b3398b7014a",
+    "url": "/grimorio-danda/static/js/1.39dc3829.chunk.js"
+  },
+  {
+    "revision": "3454856a5ccbe1ec4c42",
+    "url": "/grimorio-danda/static/js/runtime~main.3454856a.js"
+  },
+  {
+    "revision": "739cf522e450259cd381",
+    "url": "/grimorio-danda/static/js/main.739cf522.chunk.js"
+  },
+  {
+    "revision": "cbfd0298f008558557418d19abfc2f42",
+    "url": "/grimorio-danda/static/media/rpgawesome-webfont.cbfd0298.eot"
+  },
+  {
+    "revision": "bc54396771c64863193f7a3fe93d9cff",
+    "url": "/grimorio-danda/static/media/rpgawesome-webfont.bc543967.ttf"
+  },
+  {
+    "revision": "e31a701aa9b88125d36119acb8a10482",
+    "url": "/grimorio-danda/static/media/rpgawesome-webfont.e31a701a.woff"
+  },
+  {
+    "revision": "39dc38292b3398b7014a",
+    "url": "/grimorio-danda/static/css/1.7bf54022.chunk.css"
+  },
+  {
+    "revision": "c538427072ab21e0a0a41f9e67b5387c",
+    "url": "/grimorio-danda/index.html"
+  }
+];
