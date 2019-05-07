620bc74ce6db6b52c5d91966e13d7c1ad268bfce
Antônio Marco da Silva <antonio.silva@bludata.com.br>
2019-05-07 14:04:16 -0300

    removido DevTools, atualizado package

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,6 +1,5 @@
 import { AppState } from "./index";
 import { Store, createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { routerMiddleware } from "connected-react-router";
 import { History } from "history";
@@ -10,14 +9,13 @@ const configureStore = (
   history: History,
   preloadedState?: AppState
 ): Store<AppState> => {
-  const composeEnhancers = composeWithDevTools({});
 
   const middlewares = [thunk, routerMiddleware(history)];
 
   const store = createStore(
     createRootReducer(history),
     preloadedState,
-    composeEnhancers(applyMiddleware(...middlewares))
+    applyMiddleware(...middlewares)
   );
   return store;
 };
